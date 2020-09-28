a06689640d845d576f3dba461b4e7a904af42379
zderApfel <scoutt172@yahoo.com>
2020-09-28 19:47:58 -0400

    Shows rank, profile picture, and status

--- src/views/user/MyProfile.vue
+++ src/views/user/MyProfile.vue
@@ -1,10 +1,14 @@
 <template>
 <div>
-  <div class="text-left">
-    <h1><strong>{{currentUser.username}}</strong> - 
-      <span v-if="currentUser.roles.includes('[17th] Member')">17th Brigade</span>
-      <span v-else>Iceberg Member</span>    
+  <div class="text-center">
+    <img :src="currentUser.photoURL" :alt="currentUser.username" width=150 height=150></img>
+    <h1>
+      <span id="username">{{currentUser.username}}</span> - 
+      <span v-if="currentUser.roles.includes('[17th] Member')">17th Brigade </span>
+      <span v-else>Iceberg Member</span>
+      <span v-if="currentUser.roles.includes('[17th] Member')">[{{currentUser.bct_rank}}]</span>
     </h1>
+    <h2 id="status">{{currentUser.status}}</h2>
   </div>
   <p>{{currentUser}}</p>
 </div>
@@ -22,5 +26,11 @@ export default {
 </script>
 
 <style scoped>
+  #username{
+    font-weight: bold;
+  }
 
+  #status{
+    font-style: italic;
+  }
 </style>
