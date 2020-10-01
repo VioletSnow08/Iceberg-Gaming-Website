866ede8d41bb409501eb6e05f02dbeaeede40213
Vincent Lauro <vinniehat@gmail.com>
2020-10-01 17:35:51 -0400

    Revert "Made LOA message inclusive to ICE and 17bct members"

--- src/views/user/MyProfile.vue
+++ src/views/user/MyProfile.vue
@@ -13,7 +13,6 @@
       <vs-col vs-w="4" class="info-card">
         <vx-card>
             <h3 class="card-title">{{currentUser.username}}'s Contact Info</h3>
-            <h3 v-if="currentUser.onLOA" style="color: red">On Leave of Absence</h3>
             <vx-list :list="[
               `Email: ${currentUser.email}`,
               `Discord: ${currentUser.discord_id}`
@@ -23,6 +22,7 @@
       <vs-col v-if="currentUser.roles.includes('[17th] Member')" vs-w="4" class="info-card">
           <vx-card>
             <h3 class="card-title">17th Brigade Unit Card</h3>
+            <h3 v-if="currentUser.onLOA" style="color: red">On Leave of Absence</h3>
             <vx-tooltip text="In the future this will dictate 7th Rangers or 32nd. Ranger will be green, 32nd will be yellow">
                 <h3 style="color: green">[HoverOverMe]</h3>
               </vx-tooltip>
