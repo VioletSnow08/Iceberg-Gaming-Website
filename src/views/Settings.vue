c1fa0dbf6aa43128b705e316402fa60a0721156f
zderApfel <scoutt172@yahoo.com>
2020-10-08 18:33:58 -0400

    removed unnesseary comments

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -3,53 +3,7 @@
     <h1 class="text-center">Settings</h1>
     <h2 class="text-center">Customize your Iceberg Gaming Account</h2>
     <br>
-    <!--    <div class="vx-row">-->
-    <!--      &lt;!&ndash;      Starts the first VX ROW&ndash;&gt;-->
-    <!--      <div class="row-right">-->
-    <!--        <div v-if="currentUser.onLOA">-->
-    <!--          <vs-button @click="confirmPopup=true" color="primary">End LOA</vs-button>-->
-    <!--          <vs-popup classContent="popup-example" title="Are you sure you would like to end your current LOA"-->
-    <!--                    :active.sync="confirmPopup">-->
-    <!--            <vs-button @click="confirmEndLOA()" color="danger">End LOA</vs-button>-->
-    <!--          </vs-popup>-->
-    <!--        </div>-->
-    <!--        <div v-else>-->
-    <!--          <vs-button @click="popupActive=true" color="primary">Start LOA</vs-button>-->
-    <!--          <vs-popup classContent="popup-example"-->
-    <!--                    title="Please fill out the following details(end date and reason). Your LOA will begin immediately."-->
-    <!--                    :active.sync="popupActive">-->
-    <!--            <vs-alert :active.sync="endDateAlert" color="warning" closable icon-pack="feather" close-icon="icon-x">-->
-    <!--              Please select an end date.-->
-    <!--            </vs-alert>-->
-    <!--            <vs-alert :active.sync="reasonAlert" color="warning" closable icon-pack="feather" close-icon="icon-x">Please-->
-    <!--              type your reason for the LOA.-->
-    <!--            </vs-alert>-->
-    <!--            <br>-->
-    <!--            <datepicker :inline="true" placeholder="Select End Date" v-model="date"></datepicker>-->
-    <!--            <br>-->
-    <!--            <vs-textarea class="inputx mb-3" placeholder="Reason" v-model="reason"/>-->
-    <!--            <vs-button @click="submit()" :color="date && reason ? 'success' : 'danger'">Submit LOA</vs-button>-->
-    <!--          </vs-popup>-->
-    <!--        </div>-->
-    <!--      </div>-->
 
-    <!--      <vx-tooltip class="row-right" text="Will grab the current Discord Username and change it here too!">-->
-    <!--        <vs-button @click="getDiscordUsername" color="primary">Re-Pull Discord Username</vs-button>-->
-    <!--      </vx-tooltip>-->
-    <!--      <vx-tooltip class="row-right" text="Will grab the current Discord Profile Picture and change it here too!">-->
-    <!--        <vs-button @click="getDiscordProfilePicture" color="primary">Re-Pull Discord Profile Picture</vs-button>-->
-    <!--      </vx-tooltip>-->
-    <!--      <vs-button class="row-right" color="warning">Change Discord ID</vs-button>-->
-    <!--      <vs-button @click="statusPopup=true" class="row-right" color="primary">Change Status</vs-button>-->
-    <!--      <vs-popup classContent="popup-example"-->
-    <!--                :title="'What would you like to change your status to, ' + currentUser.username + '?'"-->
-    <!--                :active.sync="statusPopup">-->
-    <!--        <vs-input type="text" placeholder="New Status..." v-model="newStatus"></vs-input>-->
-    <!--        <br>-->
-    <!--        <vs-button @click="submitStatus(newStatus)" color="success">Submit</vs-button>-->
-    <!--      </vs-popup>-->
-    <!--    </div>-->
-    <!--    Ends the first VX ROW-->
     <vs-tabs position="left" class="tabs-shadow-none" id="profile-tabs">
 
       <!-- GENERAL -->
