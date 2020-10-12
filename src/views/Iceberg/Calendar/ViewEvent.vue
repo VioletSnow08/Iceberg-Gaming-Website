5c4ee48647363eb91fd8cae8fef70ce83392a272
Vincent Lauro <vinniehat@gmail.com>
2020-10-11 21:50:04 -0400

    Removed Vue-il8n, added the Changing Attendance Backend

--- src/views/Iceberg/Calendar/ViewEvent.vue
+++ src/views/Iceberg/Calendar/ViewEvent.vue
@@ -7,11 +7,23 @@
     {{this.$vs.loading.close()}}
     <h1>{{IcebergEvent($route.params.eventID).title}}</h1>
     <h2>Hosted by: {{user(IcebergEvent($route.params.eventID).creatorID).username}}</h2>
+    <h3>Start Date: {{new Date(IcebergEvent($route.params.eventID).startDate)}}</h3>
+    <h3>End Date: {{new Date(IcebergEvent($route.params.eventID).endDate)}}</h3>
     <h4>Going: {{IcebergEvent($route.params.eventID).going.length}}</h4>
     <h4>Maybe: {{IcebergEvent($route.params.eventID).maybe.length}}</h4>
     <h4>Declined: {{IcebergEvent($route.params.eventID).declined.length}}</h4>
     <br>
     <hr>
+    <br>
+    <div class="text-center">
+      <vs-button class="attendance-buttons" type="filled" color="success">Going</vs-button>
+      <vs-button class="attendance-buttons" color="warning" type="border">Maybe</vs-button>
+      <vs-button class="attendance-buttons" color="danger" type="border">Declined</vs-button>
+    </div>
+    <br>
+
+    <h4>Description: <span style="font-size: 15px;">{{IcebergEvent($route.params.eventID).description}}</span></h4>
+    <br>
     <div>
       <h1>Attendance</h1>
       <br>
@@ -28,6 +40,10 @@
       <br>
       <h4 v-for="declined in IcebergEvent($route.params.eventID).declined" v-bind:key="declined">{{user(declined).username}}</h4>
       </div>
+  </div>
+  <div v-else>
+    {{this.$vs.loading.close()}}
+    <vs-alert color="danger">Error: Event not found!</vs-alert>
   </div>
 </div>
 </template>
@@ -49,5 +65,8 @@ export default {
 </script>
 
 <style>
+.attendance-buttons {
+  margin-right: 10px !important;
+}
 
 </style>
