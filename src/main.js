b284b23dccba09eea5d961121e0da7374e7bffbb
Vincent Lauro <vinniehat@gmail.com>
2020-10-17 17:48:38 -0400

    Removed firebaseconfig for security reasons; Contact me for details

--- src/main.js
+++ src/main.js
@@ -16,16 +16,7 @@ import 'firebase/firestore'
 
 // INITIALIZING FIREBASE
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyA8e8TdgtWnNYl86HV3joSTOy-19h3Y3TQ',
-  authDomain: 'seventeenth-bct.firebaseapp.com',
-  databaseURL: 'https://seventeenth-bct.firebaseio.com',
-  projectId: 'seventeenth-bct',
-  storageBucket: 'seventeenth-bct.appspot.com',
-  messagingSenderId: '682700854607',
-  appId: '1:682700854607:web:ca34e54af9128afb82e13a',
-  measurementId: 'G-XS6SPLLTV2'
-}
+const firebaseConfig = require("./firebaseConfig");
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
 firebase.analytics()
