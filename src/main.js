5e6ffe342153f7557a1f01d5579c665b8b4daf34
Vincent Lauro <vinniehat@gmail.com>
2020-10-26 15:05:16 -0400

    DataDog is now setup and ready to go!

--- src/main.js
+++ src/main.js
@@ -6,15 +6,15 @@
   Author: Pixinvent
   Author URL: http://www.themeforest.net/user/pixinvent
 ==========================================================================================*/
-
-
+const { createLogger, format, transports } = require('winston');
+const firebaseConfig = require("./credentials.js").firebaseConfig;
+import "setimmediate";
 import Vue from 'vue'
 import App from './App.vue'
 import firebase from 'firebase'
 import 'firebase/firestore'
 import axios from './axios.js'
 import Vuesax from 'vuesax'
-const firebaseConfig = require("./firebaseConfig.js");
 import 'material-icons/iconfont/material-icons.css' //Material Icons
 import 'vuesax/dist/vuesax.css' // Vuesax
 import './filters/filters.js'
@@ -28,11 +28,27 @@ import { VueHammer } from 'vue2-hammer'
 import 'prismjs'
 import 'prismjs/themes/prism-tomorrow.css'
 require('./assets/css/iconfont.css')
-
-
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
 firebase.analytics()
+
+
+const httpTransportOptions = {
+  host: 'http-intake.logs.datadoghq.com',
+  path: "/v1/input/b6b4c63a6b8eec338b8617ae9495e173?ddsource=nodejs&service=Iceberg Gaming Website",
+  ssl: true
+};
+const logger = createLogger({
+  level: 'info',
+  exitOnError: false,
+  format: format.json(),
+  transports: [
+    new transports.Http(httpTransportOptions),
+  ],
+});
+
+
+Vue.prototype.$logger = logger;
 
 
 Vue.use(Vuesax)
