75b375f6937a2dcd58c6166910824f4c3cbbcf54
Vincent Lauro <vinniehat@gmail.com>
2020-10-29 23:57:42 -0400

    Init forum.js

---
+++ src/store/modules/channels/forum.js
@@ -0,0 +1,26 @@
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+import router from '@/router/router'
+import {logger, alertWarn} from "@/misc.js";
+const state = {
+
+}
+
+const getters = {
+
+}
+
+const actions = {
+
+}
+
+const mutations = {
+
+}
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+}
