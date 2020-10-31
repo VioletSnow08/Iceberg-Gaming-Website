c41e9c7f0c6daf4bdd90cac731a5622d4d4aba51
Vincent Lauro <vinniehat@gmail.com>
2020-10-31 02:02:03 -0400

    Cleared out Channel.js File; Added getters

--- src/store/modules/channel.js
+++ src/store/modules/channel.js
@@ -14,128 +14,38 @@ function formatDate(date) {
   return [year, month, day].join('-');
 }
 
-
 const state = {
-  events: null
+  channels: null
 }
 const getters = {
-  IcebergEvents: (state) => {
-    let events = [];
-    state.events.forEach(event => {
-      if (event.division === "Iceberg") {
-        events.push(event);
-      }
-    })
-    return events;
+  channels: (state) => {
+    return state.channels;
   },
-  BCTEvents: (state) => {
-    let events = [];
-    state.events.forEach(event => {
-      if (event.division === "17th") {
-        events.push(event);
-      }
-    })
-    return events;
+  channel: (state) => (channelID) => {
+    return state.channels.find(channel => channel.id === channelID);
   },
-  events: (state) => {
-    return state.events;
+  events: (state) => (channelID) => {
+    return state.channels.find(channel => channel.id === channelID).events;
   },
-  event: (state) => (eventID) => {
-    return state.events.find(event => event.division === "Iceberg" && event.id === eventID);
+  event: (state) => (channelID, eventID) => {
+    return state.channels.find(channel => channel.id === channelID).events.find(event => event.id === eventID);
   },
+  threads: (state) => (channelID) => {
+    return state.channels.find(channelID).threads;
+  },
+  thread: (state) => (channelID, threadID) => {
+    return state.channels.find(channel => channel.id === channelID).threads.find(thread => thread.id === threadID);
+  }
 }
 const actions = {
-  async fetchEvents({commit}) {
-    let list = [];
-    await firebase.firestore().collection('events').get().then(async events => {
-      for (const event of events.docs) {
-        const object = {...event.data(), id: event.id}
-        list.push(object);
-      }
-    })
-    commit("setEvents", list);
-  },
-  async addEvent({commit}, event) {
-    event.creatorID = firebase.auth().currentUser.uid;
-    await firebase.firestore().collection('events').doc().set(event).catch(error => {
-      if (error) throw error;
-    })
-    await this.dispatch("fetchEvents");
-  },
-  async setAttendance({commit}, [eventID, attendance]) {
-    await firebase.firestore().collection('events').doc(eventID).get().then(async event => {
-      if (event.data()) {
-        await firebase.firestore().collection('events').doc(eventID).update({
-          going: event.data().going.filter(person => person !== firebase.auth().currentUser.uid),
-          maybe: event.data().maybe.filter(person => person !== firebase.auth().currentUser.uid),
-          declined: event.data().declined.filter(person => person !== firebase.auth().currentUser.uid)
-        }).catch(update => {
-          if (update) throw update
-        })
-      }
-    }).catch(error => {
-      if (error) throw error;
-    })
 
-    await firebase.firestore().collection('events').doc(eventID).get().then(async event => {
-      if (event.data()) {
-        if (attendance === "going") {
-          let newArray = event.data().going;
-          newArray.push(firebase.auth().currentUser.uid);
-          await firebase.firestore().collection('events').doc(eventID).update({
-            going: newArray
-          })
-        } else if (attendance === "maybe") {
-          let newArray = event.data().maybe;
-          newArray.push(firebase.auth().currentUser.uid);
-          await firebase.firestore().collection('events').doc(eventID).update({
-            maybe: newArray
-          })
-        } else if (attendance === "declined") {
-          let newArray = event.data().declined;
-          newArray.push(firebase.auth().currentUser.uid);
-          await firebase.firestore().collection('events').doc(eventID).update({
-            declined: newArray
-          })
-        }
-      }
-      await this.dispatch("fetchEvents");
-    })
-  },
-  async editEvent({commit}, [eventID, title, label, startDate, endDate, style]) {
-    if(title) {
-      await firebase.firestore().collection('events').doc(eventID).update({
-        title
-      })
-    }
-    if(label) {
-      await firebase.firestore().collection('events').doc(eventID).update({
-        label
-      })
-    }
-    if(startDate) {
-      await firebase.firestore().collection('events').doc(eventID).update({
-        startDate
-      })
-    }
-    if(endDate) {
-      await firebase.firestore().collection('events').doc(eventID).update({
-        endDate
-      })
-    }
-    if(style) {
-      await firebase.firestore().collection('events').doc(eventID).update({
-        style
-      })
-    }
-    await this.dispatch("fetchEvents");
-  }
 }
 const mutations = {
-  setEvents(state, events) {
-    state.events = events;
+  setChannels(state, channels) {
+    state.channels = channels;
   }
 }
+
 export default {
   state, getters, actions, mutations
 }
