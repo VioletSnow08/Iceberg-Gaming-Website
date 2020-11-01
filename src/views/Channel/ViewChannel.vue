f69646f31f0e87daa3cf7a708cfd7688efd04411
Vincent Lauro <vinniehat@gmail.com>
2020-11-01 01:10:38 -0500

    Setup a test ViewChannel.Vue File

--- src/views/Channel/ViewChannel.vue
+++ src/views/Channel/ViewChannel.vue
@@ -1,12 +1,33 @@
 <template>
-<div>
-  <h1>Viewing a channel!</h1>
-</div>
+  <div>
+    <div v-if="!channels">
+      <h1>{{ this.$vs.loading({type: "radius", text: "Loading Channel..."}) }}</h1>
+    </div>
+    <div v-else>
+      {{this.$vs.loading.close()}}
+      <Calendar v-if="channel(channelID) && channel(channelID).isCalendar" />
+    </div>
+  </div>
 </template>
 
 <script>
+import {mapGetters} from "vuex";
+import Calendar from "./Calendar/Calendar";
+
 export default {
-name: "ViewChannel"
+  name: "ViewChannel",
+  props: ["channelID"],
+  components: {
+    Calendar
+  },
+  computed: {
+    ...mapGetters(["channels", "channel"])
+  },
+  async created () {
+    await Promise.all([
+      this.$store.dispatch('fetchChannels'),
+    ])
+  },
 }
 </script>
 
