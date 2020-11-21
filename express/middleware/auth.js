cfd18160996cbd9abcdac7677ceb11d597e682c3
Vincent Lauro <vinniehat@gmail.com>
2020-11-20 22:14:57 -0500

    Modified Auth Middleware to fix bugs and log properly for invalid tokens

--- express/middleware/auth.js
+++ express/middleware/auth.js
@@ -4,34 +4,34 @@ const {logger} = require("../../utils");
 
 
 
-const requiresAuth = (req, res, next) => {
+async function requiresAuth(req, res, next) {
   const accessToken = req.body.accessToken;
   const con = req.app.get('con');
   if(accessToken) {
     jwt.verify(accessToken, jwt_secret, async (error, decodedToken) => {
       if(error) {
         logger.log({
-          level: "emergency",
+          level: "info",
           message: error.message,
-          stack: error.stack,
           isLoggedIn: false,
           id: null,
           api: "Requires Auth - Middleware"
         })
-        res.sendStatus(500);
+        res.sendStatus(400);
       } else {
         await con.query(`SELECT * FROM users WHERE id = ?`, [decodedToken.id]).then(rows => {
           if(rows[0]) {
+            req.userID = decodedToken.id;
             next();
           } else {
             res.status(401).send("Invalid accessToken!");
           }
-        }).catch(error => {
-          if(error) {
+        }).catch(e => {
+          if(e) {
             logger.log({
               level: "emergency",
-              message: error.message,
-              stack: error.stack,
+              message: e.message,
+              stack: e.stack,
               isLoggedIn: false,
               id: null,
               api: "Requires Auth - Middleware"
