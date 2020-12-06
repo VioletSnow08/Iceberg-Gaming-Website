342b91f08d480ea0d03212842dabc4dc4867e6b0
Vincent Lauro <vinniehat@gmail.com>
2020-12-06 18:53:16 -0500

    Added 2 headers to the applications page

--- src/views/admin/Applications.vue
+++ src/views/admin/Applications.vue
@@ -4,40 +4,48 @@
       <h1>{{ this.$vs.loading({type: "radius", text: "Loading Applications..."}) }}</h1>
     </div>
 
-    <div v-else class="vx-row">
-      {{this.$vs.loading.close()}}
-
-      <div v-for="app in applications" v-bind:key="app.id" class="vx-col w-full lg:w-1/4 sm:w-1/2 mb-base">
-        <vx-card :title="user(app.userID).username" :subtitle="'Division: ' + app.division" class="p-2">
-          <div class="text-center">
-            <vs-chip v-if="app.status.toLowerCase() === 'accepted'" color="success">{{app.status}}</vs-chip>
-            <vs-chip v-else-if="app.status.toLowerCase() === 'waiting'" color="warning">{{app.status}}</vs-chip>
-            <vs-chip v-else-if="app.status.toLowerCase() === 'processing'" color="warning">{{app.status}}</vs-chip>
-            <vs-chip v-else-if="app.status.toLowerCase() === 'denied'" color="danger">{{app.status}}</vs-chip>
-          </div>
-<!--          <vs-avatar :src="user(app.user_id).photoURL" class="mx-auto my-6 block" size="80px"/>-->
-          <vs-button v-if="app.division === '17th'" @click="viewApplication(app.userID, app.id)" type="gradient" class="w-full mt-6" color="#FF0099" gradient-color-secondary="#493240">View Application</vs-button>
-          <vs-button v-else-if="app.division === 'Iceberg'" @click="viewApplication(app.userID, app.id)" type="gradient" class="w-full mt-6" color="#a8c0ff" gradient-color-secondary="#3f2b96">View Application</vs-button>
-        </vx-card>
+    <div v-else>
+      {{ this.$vs.loading.close() }}
+      <h1 class="text-center">Applications</h1>
+      <h2 class="text-center">Accept, deny, and process applications here</h2>
+      <div>
+        <div v-for="app in applications" v-bind:key="app.id" class="vx-col w-full lg:w-1/4 sm:w-1/2 mb-base">
+          <vx-card :title="user(app.userID).username" :subtitle="'Division: ' + app.division" class="p-2">
+            <div class="text-center">
+              <vs-chip v-if="app.status.toLowerCase() === 'accepted'" color="success">{{ app.status }}</vs-chip>
+              <vs-chip v-else-if="app.status.toLowerCase() === 'waiting'" color="warning">{{ app.status }}</vs-chip>
+              <vs-chip v-else-if="app.status.toLowerCase() === 'processing'" color="warning">{{ app.status }}</vs-chip>
+              <vs-chip v-else-if="app.status.toLowerCase() === 'denied'" color="danger">{{ app.status }}</vs-chip>
+            </div>
+            <!--          <vs-avatar :src="user(app.user_id).photoURL" class="mx-auto my-6 block" size="80px"/>-->
+            <vs-button v-if="app.division === '17th'" @click="viewApplication(app.userID, app.id)" type="gradient"
+                       class="w-full mt-6" color="#FF0099" gradient-color-secondary="#493240">View Application
+            </vs-button>
+            <vs-button v-else-if="app.division === 'Iceberg'" @click="viewApplication(app.userID, app.id)"
+                       type="gradient" class="w-full mt-6" color="#a8c0ff" gradient-color-secondary="#3f2b96">View
+              Application
+            </vs-button>
+          </vx-card>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import {mapGetters} from 'vuex'
+import {mapActions, mapGetters} from 'vuex'
 
 export default {
   name: 'Applications',
   methods: {
-    viewApplication (userID, applicationID) {
+    viewApplication(userID, applicationID) {
       this.$router.push({name: 'AdminApplication', params: {userID, applicationID}})
     },
   },
   computed: {
-    ...mapGetters(['applications', 'currentUser', 'user'])
+    ...mapGetters(['applications', 'currentUser', 'user']),
   },
-  async created () {
+  async created() {
     await Promise.all([
       this.$store.dispatch('fetchApplications'),
       this.$store.dispatch('fetchUsers')
