a9996a0b82e57e784f1d92c529797285c48aa34c
Vincent Lauro <vinniehat@gmail.com>
2020-12-10 19:35:35 -0500

    Created the Accept, Deny, and Process 17th Applications API

--- express/modules/applications.js
+++ express/modules/applications.js
@@ -76,7 +76,7 @@ router.post('/create/:division', async (req, res) => {
   division = division.toLowerCase();
   console.log(division)
   if (!accessToken || !division) return res.status(400).send("Bad Request! Please provide an accessToken and a division(17th, Iceberg, or CGS)");
-  if (division === "17th" && !utils.doesUserContainRolesAuth(req.user.roles, "[17th] Member")) {
+  if (division === "17th" && !utils.doesUserContainRoles(req.user.roles, "[17th] Member")) {
     let {
       steamURL,
       timezone,
@@ -94,7 +94,7 @@ router.post('/create/:division', async (req, res) => {
       attendOps
     } = req.body;
     if ((steamURL || timezone || age || arma3Hours || hobbies || whyJoin || attractmilsim || ranger || medic || sapper || pilot || tank_crew || idf || attendOps) === undefined) return res.status(400).send("Bad Request! Please provide ALL questions in the body.");
-    if((ranger || medic || sapper || pilot || tank_crew || idf || attendOps) !== (true || false)) return res.status(400).send("Bad Request! Please provide ALL questions in the body.");
+    if ((ranger || medic || sapper || pilot || tank_crew || idf || attendOps) !== (true || false)) return res.status(400).send("Bad Request! Please provide ALL questions in the body.");
     let createdAt = DateTime.local().setZone('America/Chicago').toFormat('yyyy-MM-dd HH:mm:ss');
     con.query(`INSERT INTO 17th_applications (userID, createdAt, steamURL, timezone, age, arma3Hours, hobbies, whyJoin, attractmilsim, ranger, medic, sapper, pilot, tank_crew, idf, attendOps) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, [userID, createdAt, steamURL, timezone, age, arma3Hours, hobbies, whyJoin, attractmilsim, ranger, medic, sapper, pilot, tank_crew, idf, attendOps]).then(row => {
       let rowID = row.insertId;
@@ -119,14 +119,21 @@ router.post('/create/:division', async (req, res) => {
       }
       res.json(safeApplication);
     }).catch(error => {
-      if(error) {
+      if (error) {
         console.log(error);
         res.sendStatus(500);
       }
     })
-  } else if(division === "iceberg" && !utils.doesUserContainRolesAuth(req.user.roles, "[ICE] Member")) {
+  } else if (division === "iceberg" && !utils.doesUserContainRoles(req.user.roles, "[ICE] Member")) {
     let {
-      steamURL, age, hobbies, gamesTheyJoinFor, hoursInGamesTheyJoinFor, areYouInAnyCommunities, whyJoin, whereDidYouHearUsFrom
+      steamURL,
+      age,
+      hobbies,
+      gamesTheyJoinFor,
+      hoursInGamesTheyJoinFor,
+      areYouInAnyCommunities,
+      whyJoin,
+      whereDidYouHearUsFrom
     } = req.body;
     let createdAt = DateTime.local().setZone('America/Chicago').toFormat('yyyy-MM-dd HH:mm:ss');
     con.query(`INSERT INTO iceberg_applications (userID, createdAt, steamURL, age, hobbies, gamesTheyJoinFor, hoursInGamesTheyJoinFor, areYouInAnyCommunities, whyJoin, whereDidYouHearUsFrom) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, [userID, createdAt, steamURL, age, hobbies, gamesTheyJoinFor, hoursInGamesTheyJoinFor, areYouInAnyCommunities, whyJoin, whereDidYouHearUsFrom]).then(row => {
@@ -147,12 +154,111 @@ router.post('/create/:division', async (req, res) => {
       }
       res.json(safeApplication)
     }).catch(error => {
-      if(error) {
+      if (error) {
         console.log(error);
         res.sendStatus(500);
       }
     })
+  } else {
+    res.status(400).send("Bad Request! Invalid Division or in-proper roles.");
   }
+})
+
+// POST: /api/v1/applications/<action>/<id>
+// Params: action, id, division
+// Body: accessToken
+// Return: application
+
+router.delete('/change/', async (req, res) => {
+  let {accessToken} = req.body;
+  console.log(req.user.roles);
+  let {action, id, division} = req.body;
+  const userID = req.user.id;
+  const con = req.app.get('con');
+  const api = "/api/v1/settings/applications/" + action;
+  let comment;
+  let status;
+  if (!accessToken || !action || !id || !division) return res.status(400).send("Bad Request! Please provide an accessToken, action, division, and id");
+  if (utils.doesUserContainRoles(req.user.roles, ["[ICE] Recruiter", "[ICE] Admin", "[ICE] Owner"])) {
+    if (division.toLowerCase() === "17th") {
+      if (action.toLowerCase() === "approve") {
+        comment = "Hello,\n" +
+          "\n" +
+          "Your application to the 17th Brigade Combat Team has been accepted! It is our great pleasure to welcome you to the community. \n" +
+          "\n" +
+          "There are only a couple of things to take care of in order to join us on the battlefield: \n" +
+          "\n" +
+          "First, you need to obtain the modpack that we use on our private server. It can take a little while to download everything, so try to get it started as soon as possible. The following link will take you to our \"shadow mod\" which acts as a dependency node for all of the mods we use. In order to get the mod pack, simply subscribe to the Shadow Mod, and when it asks if you want to download all mods associated, say yes.\n" +
+          "\n" +
+          `${utils.data.bct_modpack}\n` +
+          "\n" +
+          "Second, once you have your modpack, look for anybody that is Corporal or above on TeamSpeak or make a post on the Discord and someone will get you set up. \n" +
+          "\n" +
+          `TS: ${utils.data.teamspeak}\n` +
+          `Discord: ${utils.data.discord}\n` +
+          "\n" +
+          "They will be able to help you or find someone to help you with initial assessments and/or training. Basic Training is held weekly on Tuesdays, but don't let that stop you from jumping in during the week and Saturday op to play with us!\n" +
+          "\n" +
+          "Finally, please take the time to familiarize yourself with our Code of Conduct, Ranks, and Structure pages. You might also take the time to browse our forums and introduce yourself.\n" +
+          "\n" +
+          "If you have any further questions or issues, you are more than welcome to ask anyone on Teamspeak, Discord, or even Enjin and we will help out as best as we can!\n" +
+          "\n" +
+          "Regards,\n" +
+          "\n" +
+          `${req.user.username}`
+        status = "Approved";
+      } else if (action.toLowerCase() === "deny") {
+        comment = "Hello,\n" +
+          "We have reviewed your application and we thank you for your interest in joining the 17th Brigade Combat Team. However, we are regrettably unable to accept your application at this time. Please feel free to inquire why your application was not approved. You are free to resubmit your application in the future when we may be better able to incorporate you.\n" +
+          "\n" +
+          "All the best.\n" +
+          "\n" +
+          "-Staff\n";
+        status = "Denied";
+      } else if (action.toLowerCase() === "process") {
+        comment = "Hello,\n" +
+          "\n" +
+          `Your application has been processed and the recruiter has requested an interview. When you're able to, hop on to the TeamSpeak (${utils.data.teamspeak}) and reach out to a recruiter! They'll be able to answer your questions and fill you in how things work here.\n` +
+          "\n" +
+          "Thanks,\n" +
+          "\n" +
+          `${req.user.username}`
+        status = "Processed";
+      }
+      if(status && comment) {
+        con.query(`UPDATE 17th_applications SET comment = ?, status = ? WHERE id = ?`, [comment, status, id]).then(() => {
+          res.sendStatus(200);
+          utils.logger.log({
+            level: "info",
+            message: "Application Status Changed",
+            status,
+            comment,
+            userID,
+            id,
+            division,
+            api,
+            action
+          })
+        }).catch(error => {
+          if(error) {
+            res.sendStatus(500)
+            utils.logger.log({
+              level: "error",
+              message: error.message,
+              error: error.stack,
+              status,
+              comment,
+              userID,
+              id,
+              division,
+              api,
+              action
+            })
+          }
+        })
+      } else res.status(400).send("Bad Request! Please provide a valid action!")
+    } else res.status(400).send("Bad Request! Please provide a valid division!");
+  } else res.status(401).send("Unauthorized!");
 })
 
 module.exports = {
--- src/store/modules/application.js
+++ src/store/modules/application.js
@@ -49,6 +49,7 @@ const actions = {
       accessToken: await rootGetters.currentUser.accessToken
     }).then(async response => {
       await this.dispatch('fetchCurrentUser');
+      await router.push('/user/applications');
     }).catch(error => {
       if(error) {
         utils.alertGeneral();
@@ -69,6 +70,7 @@ const actions = {
       accessToken: await rootGetters.currentUser.accessToken
     }).then(async response => {
       await this.dispatch('fetchCurrentUser');
+      await router.push('/user/applications');
     }).catch(error => {
       if(error) {
         utils.alertGeneral();
@@ -87,103 +89,21 @@ const actions = {
       }
     })
   },
-  async changeApplicationStatus({commit}, [newStatus, userID, applicationID, division]) {
-    if (firebase.auth().currentUser) {
-      await firebase.firestore().collection("users").doc(firebase.auth().currentUser.uid).get().then(async doc => {
-        if (doc.data().roles.includes("[ICE] Recruiter")) {
-          let newComment = "";
-
-          if (division === "17th") {
-            if (newStatus.toLowerCase() === "processing") {
-              newComment = "Your application is currently being held for processing. When you're able to, we request that we interview you(nothing bad!) and that you hop on to the TeamSpeak (ts.iceberg-gaming.com) to reach out to a recruiter! They'll be able to answer your questions and fill you in how things work here."
-            } else if (newStatus.toLowerCase() === "accepted") {
-              newComment = "Congrats! Your application to the 17th Brigade Combat Team has been accepted! It is our great pleasure to welcome you to the community. " +
-                "There are only a couple of things to take care of in order to join us on the battlefield. First we request that you subscribe our shadow mod. You can subscribe to our shadow mod here: " +
-                "https://steamcommunity.com/sharedfiles/filedetails/?id=1501538330" +
-                " . Second, we request that you hop on the Teamspeak (ts.iceberg-gaming.com) and message a Corporal(CPL) or above to get your roles. If nobody is available, make sure you post it in general chat on our Discord." +
-                " Last but not least, we ask that you familiarize yourself with our Code of Conduct, Ranks, and Structure Pages. If you happen to have any questions please feel free to ask around. Until then, we'll see you soon!"
-              await this.dispatch("acceptUser", [userID, division]);
-            } else if (newStatus.toLowerCase() === "denied") {
-              newComment = "We have reviewed your application and we thank you for your interest in the 17th Brigade Combat Team. However, unfortunately we were unable to accept your application at this time. You may inquire about why your application was declined." +
-                " You are free to resubmit your application in the future at any time. We are sorry about the inconvenience.";
-            }
-
-            await firebase.firestore().collection("users").doc(userID).collection("applications").doc(applicationID).update({
-              status: newStatus,
-              comment: newComment
-            }).then(async () => {
-              console.log("updated");
-              utils.logger.log({
-                level: "info",
-                message: "Application Status Changed",
-                recruiter: firebase.auth().currentUser.uid,
-                isLoggedIn: true,
-                userID,
-                applicationID,
-                division,
-                newStatus
-              })
-            }).catch(error => {
-              if(error) {
-                utils.logger.log({
-                  level: "alert",
-                  message: error.message,
-                  stack: error.stack,
-                  recruiter: firebase.auth().currentUser.uid,
-                  isLoggedIn: true,
-                  userID,
-                  applicationID,
-                  division,
-                  newStatus
-                })
-                utils.alertGeneral()
-              }
-            })
-          } else if (division === "Iceberg") {
-            if (newStatus.toLowerCase() === "processing") {
-              newComment = "Your application is currently being held for processing. When you're able to, we request that we interview you(nothing bad!) and that you hop on to the TeamSpeak (ts.iceberg-gaming.com) to reach out to a recruiter! They'll be able to answer your questions and fill you in how things work here.";
-            } else if (newStatus.toLowerCase() === "accepted") {
-              newComment = "";
-              await this.dispatch("acceptUser", [userID, division]);
-            } else if (newStatus.toLowerCase() === "denied") {
-              newComment = "We have reviewed your application and we thank you for your interest in Iceberg Gaming. However, unfortunately we were unable to accept your application at this time. You may inquire about why your application was declined." +
-                " You are free to resubmit your application in the future at any time. We are sorry about the inconvenience.";
-            }
-
-            await firebase.firestore().collection("users").doc(userID).collection("applications").doc(applicationID).update({
-              status: newStatus,
-              comment: newComment
-            }).then(async () => {
-              utils.logger.log({
-                level: "info",
-                message: "Application Status Changed",
-                recruiter: firebase.auth().currentUser.uid,
-                isLoggedIn: true,
-                userID,
-                applicationID,
-                division,
-                newStatus
-              })
-            }).catch(error => {
-              if(error) {
-                utils.logger.log({
-                  level: "alert",
-                  message: error.message,
-                  stack: error.stack,
-                  recruiter: firebase.auth().currentUser.uid,
-                  isLoggedIn: true,
-                  userID,
-                  applicationID,
-                  division,
-                  newStatus
-                })
-                utils.alertGeneral()
-              }
-            })
-          }
-        }
-      })
-    }
+  async changeApplicationStatus({commit, rootGetters}, [newStatus, userID, applicationID, division]) {
+    axios.delete(`${utils.base_url}/applications/change`, {
+      data: {
+        action: newStatus,
+        division,
+        id: applicationID,
+        accessToken: await rootGetters.currentUser.accessToken
+      }
+    }).then(response => {
+      this.dispatch('fetchApplications');
+    }).catch(error => {
+      if(error) {
+        utils.alertGeneral();
+      }
+    })
   }
 
 }
