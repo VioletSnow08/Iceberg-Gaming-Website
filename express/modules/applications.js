9358c9de042c988744bbef5f642a92690a33fa8c
Vincent Lauro <vinniehat@gmail.com>
2020-12-13 18:19:12 -0500

    Added Application Responses for Iceberg Gaming

--- express/modules/applications.js
+++ express/modules/applications.js
@@ -259,7 +259,30 @@ router.delete('/change/', async (req, res) => {
         })
       }
     } else if(division.toLowerCase() === "iceberg") {
-
+      if(action.toLowerCase() === "approve") {
+        comment = "Welcome to Iceberg Gaming! We're glad to have you aboard our community and hope you can feel at home in our community. \n" +
+          "\n" +
+          `If you haven't already, we highly recommend you join our Teamspeak (${utils.data.teamspeak}) and Discord (${utils.data.discord}) so you can play with the rest of the community. Feel free to chat in any of the channels with all the members of the community, and don't be afraid to explore some of the other games that we play in Iceberg Gaming. Keep yourself updated on the Guilded Calendar for any Iceberg events and don't be afraid to gather some people and schedule something of your own.\n` +
+          "\n" +
+          "Lastly, be sure to read up on the Community Code of Conduct so you make sure you can continue to have a good time in the community.\n" +
+          "If you have any further questions or issues, you are more than welcome to ask anyone on Teamspeak, Discord, or even Guilded and we will help out as best as we can!\n" +
+          "Regards, \n" + req.user.username;
+        status = "Approved";
+      } else if(action.toLowerCase() === "process") {
+        comment = "Hello,\n" +
+          "\n" +
+          `Your application has been processed and the recruiter has requested an interview. When you're able to, hop on to the TeamSpeak (${utils.data.teamspeak}) and reach out to a recruiter! They'll be able to answer your questions and fill you in how things work here.\n` +
+          "\n" +
+          "Thanks,\n" +
+          "\n" +
+          `${req.user.username}`
+        status = "Processed";
+      } else if(action.toLowerCase() === "deny") {
+        comment = "We have reviewed your application and we thank you for your interest in joining Iceberg Gaming. However, we are regrettably unable to accept your application at this time. Please feel free to inquire why your application was not approved. You are free to resubmit your application in the future when we may be better able to incorporate you.\n" +
+          "All the best.\n" +
+          "-Staff";
+        status = "Denied";
+      }
     } else if(division.toLowerCase() === "cgs") {
 
     } else res.status(400).send("Bad Request! Please provide a valid division!");
