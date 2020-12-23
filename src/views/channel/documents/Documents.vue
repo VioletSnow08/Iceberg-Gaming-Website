acb0a17f62699a825979d7b1a67b27ef5f516d4f
Vincent Lauro <vinniehat@gmail.com>
2020-12-23 13:06:19 -0500

    Creating the Documents Files

---
+++ src/views/channel/documents/Documents.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <h1>Documents Channel</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Documents"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/channel/documents/ViewDocument.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <h1>Now Viewing a Document</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: "ViewDocument"
+}
+</script>
+
+<style scoped>
+
+</style>
