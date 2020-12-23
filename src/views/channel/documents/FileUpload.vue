d5d713546c298fc37ec4ae38801230ccbdb3927f
Vincent Lauro <vinniehat@gmail.com>
2020-12-23 13:22:12 -0500

    Create FileUpload

---
+++ src/views/channel/documents/FileUpload.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="file">
+    <form enctype="multipart/form-data">
+      <div class="fields">
+        <label>Upload File</label>
+        <input type="file" />
+      </div>
+      <div class="fields">
+        <vs-button>Upload</vs-button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FileUpload"
+}
+</script>
+
+<style scoped>
+
+</style>
