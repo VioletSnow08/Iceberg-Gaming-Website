f27d980972ce62dee6c82acc757125358aab8409
Vincent Lauro <vinniehat@gmail.com>
2020-12-24 20:40:21 -0500

    Created fetching and computed methods for the Documents component

--- src/views/channel/documents/Documents.vue
+++ src/views/channel/documents/Documents.vue
@@ -4,7 +4,6 @@
       <vs-button icon="add" @click="openFileUpload=true">Upload Document</vs-button>
     </div>
 
-
     <vs-popup :active.sync="openFileUpload" title="Upload Document">
       <FileUpload v-bind:channelID="$route.params.channelID"></FileUpload>
     </vs-popup>
@@ -13,16 +12,26 @@
 
 <script>
 import FileUpload from "@/views/channel/documents/FileUpload";
+import {mapGetters} from "vuex";
 
 export default {
   name: "Documents",
   components: {
     FileUpload
   },
+  computed: {
+    ...mapGetters(["users", "user", "channels", "documents", "document"])
+  },
   data() {
     return {
       openFileUpload: false
     }
+  },
+  async created() {
+    await Promise.all([
+      this.$store.dispatch('fetchChannels'),
+      this.$store.dispatch('fetchUsers')
+    ])
   }
 }
 </script>
