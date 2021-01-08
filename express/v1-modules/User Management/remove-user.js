509e5d431a392b419f143f55878761ee417ee17e
Vincent Lauro <vinniehat@gmail.com>
2021-01-07 20:46:33 -0500

    Added DataDog Logging to the Remove-User 17th BCT API

--- express/v1-modules/User Management/remove-user.js
+++ express/v1-modules/User Management/remove-user.js
@@ -13,22 +13,59 @@ module.exports.index = function(req, res, VALID_REMOVE_ROLES, canUserRemoveUser)
     })
     switch(division.toLowerCase()) {
       case "iceberg":
-        removeUserFrom17th({loggedInUserID: req.user.id, currentRoles}, {userID, usersRoles}, canUserRemoveUser, con);
+        removeUserFrom17th({loggedInUserID: req.user.id, currentRoles}, {userID, usersRoles}, canUserRemoveUser, con, api);
     }
   })
 }
 
 
-function removeUserFrom17th({loggedInUserID, currentRoles}, {userID, usersRoles}, canUserRemoveUser, con) {
+function removeUserFrom17th({loggedInUserID, currentRoles}, {userID, usersRoles}, canUserRemoveUser, con, api) {
   if(canUserRemoveUser(currentRoles, "17th", usersRoles)) {
     usersRoles.forEach(role => {
       if(role.startsWith("[17th]")) {
-        con.query(`DELETE FROM user_roles WHERE userID = ? AND role = ?`, [userID, role]).catch(error => {
+        con.query(`DELETE FROM user_roles WHERE userID = ? AND role = ?`, [userID, role]).then(() => {
+          utils.logger.log({
+            level: "info",
+            message: "Role Removed",
+            isLoggedIn: true,
+            userID: loggedInUserID,
+            currentRoles,
+            other: {
+              removedFrom: userID,
+              roles: usersRoles
+            },
+            api
+          })
+        }).catch(error => {
           if(error) {
-            console.log(error)
+            utils.logger.log({
+              level: "error",
+              message: error.message,
+              stack: error.stack,
+              isLoggedIn: true,
+              userID: loggedInUserID,
+              api,
+              currentRoles,
+              other: {
+                removedFrom: userID,
+                roles: usersRoles
+              }
+            })
           }
         })
       }
+    })
+    utils.logger.log({
+      level: "info",
+      message: "User removed",
+      division: "17th",
+      isLoggedIn: true,
+      userID: loggedInUserID,
+      currentRoles,
+      other: {
+        removedUser: userID
+      },
+      api
     })
   }
 }
