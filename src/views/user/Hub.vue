584b825c4c5a79581a89315fe51f4f43978a522b
Vincent Lauro <vinniehat@gmail.com>
2021-01-12 15:39:07 -0500

    Reversed and added current LOA status updated/fixed bug to the hub

--- src/views/user/Hub.vue
+++ src/views/user/Hub.vue
@@ -1,81 +1,38 @@
 <template>
   <div>
-    <div class="text-center">
-      <h1>Welcome to the Iceberg Gaming Hub!</h1>
-      <h2>Enjoy your stay!</h2>
+    <div v-if="!users">
+      <h1>{{ this.$vs.loading({type: "radius", text: "Loading The Hub..."}) }}</h1>
     </div>
-    <br>
-    <!--    Row 2 - Iceberg Gaming Related-->
-    <div v-if="currentUser.roles.includes('[ICE] Member')" class="vx-row">
-      <div class="vx-col w-full sm:w-1/2 md:w-1/2 lg:w-1/4 xl:w-1/4">
-        <statistics-card-line
-          hideChart
-          class="mb-base"
-          icon="CoffeeIcon"
-          icon-right
-          :statisticTitle="currentUser.status"
-          statistic="Current Status"/>
+    <div v-else-if="users">
+      {{this.$vs.loading.close()}}
+      <div class="text-center">
+        <h1>Welcome to the Iceberg Gaming Hub!</h1>
+        <h2>Enjoy your stay!</h2>
       </div>
-      <div class="vx-col w-full sm:w-1/2 md:w-1/2 lg:w-1/4 xl:w-1/4">
+      <br>
+      <!--    Row 2 - Iceberg Gaming Related-->
+      <div v-if="currentUser.roles.includes('[ICE] Member')" class="vx-row">
+        <div class="vx-col w-full sm:w-1/2 md:w-1/2 lg:w-1/4 xl:w-1/4">
+          <statistics-card-line
+            hideChart
+            class="mb-base"
+            icon="CoffeeIcon"
+            icon-right
+            :statisticTitle="currentUser.status"
+            statistic="Current Status"/>
+        </div>
+        <div class="vx-col w-full sm:w-1/2 md:w-1/2 lg:w-1/4 xl:w-1/4">
           <statistics-card-line
             hideChart
             class="mb-base"
             icon="EyeIcon"
             icon-right
-            :statistic="currentUser.onLOA ? 'On LOA' : 'Not on LOA'"
-            statisticTitle="LOA Status"/>
+            statistic="LOA Status"
+            :statisticTitle="isUserOnLOA([currentUser.id]) ? 'On LOA' : 'Not on LOA'"/>
+        </div>
       </div>
     </div>
 
-    <!--    Row 2 - 17th Brigade Combat Team Related-->
-    <div v-if="currentUser.roles.includes('[17th] Member')" class="vx-row">
-      <div class="vx-col w-1/2 md:w-1/3 xl:w-1/6">
-        <statistics-card-line
-          hideChart
-          class="mb-base"
-          icon="CheckIcon"
-          :statistic="currentUser.bct_rank"
-          statisticTitle="17th - Current Rank"/>
-      </div>
-      <div v-if="currentUser.bct_eligibleForPromotion" class="vx-col w-1/2 md:w-1/3 xl:w-1/6">
-        <vx-tooltip text="You are eligible for promotion!">
-          <statistics-card-line
-            hideChart
-            class="mb-base"
-            icon="ChevronsUpIcon"
-            color="success"
-            :statistic="currentUser.bct_points"
-            statisticTitle="17th - Points"/>
-        </vx-tooltip>
-      </div>
-      <div v-else-if="currentUser.bct_eligibleForDemotion" class="vx-col w-1/2 md:w-1/3 xl:w-1/6">
-        <vx-tooltip text="You are eligible for demotion!">
-          <statistics-card-line
-            hideChart
-            class="mb-base"
-            icon="ChevronsDownIcon"
-            color="danger"
-            :statistic="currentUser.bct_points"
-            statisticTitle="17th - Points"/>
-        </vx-tooltip>
-      </div>
-      <div v-else class="vx-col w-1/2 md:w-1/3 xl:w-1/6">
-        <statistics-card-line
-          hideChart
-          class="mb-base"
-          icon="CheckIcon"
-          :statistic="currentUser.bct_points"
-          statisticTitle="17th - Points"/>
-      </div>
-      <div class="vx-col w-1/2 md:w-1/3 xl:w-1/6">
-        <statistics-card-line
-          hideChart
-          class="mb-base"
-          icon="CheckSquareIcon"
-          :statistic="currentUser.bct_events_attended"
-          statisticTitle="17th - Events Attended"/>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -86,10 +43,15 @@ import StatisticsCardLine from '@/components/statistics-cards/StatisticsCardLine
 export default {
   name: "Hub",
   computed: {
-    ...mapGetters(["currentUser"])
+    ...mapGetters(["currentUser", "isUserOnLOA", "users"])
   },
   components: {
     StatisticsCardLine
+  },
+  async created () {
+    await Promise.all([
+      this.$store.dispatch('fetchUsers')
+    ])
   }
 }
 </script>
