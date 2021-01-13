8f9eab1ab70e40eecd98c2e3246bfc9f6ef0bb0a
Vincent Lauro <vinniehat@gmail.com>
2021-01-13 09:56:35 -0500

    fixed api string for disc. action review.js

--- express/v1-modules/Administrative/Disciplinary Action/review.js
+++ express/v1-modules/Administrative/Disciplinary Action/review.js
@@ -3,7 +3,7 @@ module.exports.index = function(req, res, REQUIRED_ROLES) {
   let {accessToken, comment, punishment, id} = req.body;
   const userID = req.user.id;
   const con = req.app.get('con');
-  const api = "/api/v1/administrative/disciplinary-action/view"
+  const api = "/api/v1/administrative/disciplinary-action/review"
   if (!accessToken || !comment || !id ) return res.status(400).send("Bad Request! Please pass in ALL fields in the body.");
   if(utils.doesUserContainRoles(req.user.roles, REQUIRED_ROLES)) {
     con.query(`UPDATE disciplinary_action_forms SET comment = ?, actualPunishment = ?, whoPunished = ? WHERE id = ?`, [comment, punishment, userID, id]).then(() => {
